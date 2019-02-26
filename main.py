8c409f01f4dc1fe9a23da99fb3765c46efe2f2cb
suryaprakashmereddy <sreddy@acertaralabs.com>
2019-02-25 19:35:33 -0700

    Project setup

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ matplotlib.rcParams['text.color'] = 'k'
 
 df = pd.read_excel("DataSet/Superstore.xls")
 furniture = df.loc[df['Category'] == 'Furniture']
+
+furniture['Order Date'].min(), furniture['Order Date'].max()
+
+cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City', 'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity', 'Discount', 'Profit']
+furniture.drop(cols, axis=1, inplace=True)
+furniture = furniture.sort_values('Order Date')
+furniture.isnull().sum()
+
+furniture = furniture.groupby('Order Date')['Sales'].sum().reset_index()
+
+furniture = furniture.set_index('Order Date')
+
+y = furniture['Sales'].resample('MS').mean()
+
+y.plot(figsize=(15, 6))
+plt.show()
