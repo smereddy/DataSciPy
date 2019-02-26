26e709eb640e1ae99353c4b7d5cd2c05723dcbdf
suryaprakashmereddy <sreddy@acertaralabs.com>
2019-02-25 18:43:58 -0700

    Project setup

---
+++ main.py
@@ -0,0 +1,17 @@
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
+
+df = pd.read_excel("DataSet/Superstore.xls")
+furniture = df.loc[df['Category'] == 'Furniture']
