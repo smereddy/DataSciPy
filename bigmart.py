c29cdefc27adc4c91a0fc2392e4c6d279d20f890
suryaprakashmereddy <sreddy@acertaralabs.com>
2019-04-11 22:58:56 -0600

    Bigmart project

--- bigmart.py
+++ bigmart.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import numpy as np
+import scipy as stats
+from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+from sklearn.model_selection import cross_validate, cross_val_score
+
+
 
 # Reading Data
 test = pd.read_csv('DataSet/bigmart/Test.csv')
-train = pd .read_csv('DataSet/bigmart/Train.csv')
+train = pd.read_csv('DataSet/bigmart/Train.csv')
 
 # Concatenating Training and Test Data
-data = pd.concat([train, test])
+data = pd.concat([train, test], sort=False)
 print(train.shape, test.shape, data.shape)
 
 # Finding the count of missing value in the data by col
@@ -22,3 +28,151 @@ print(data_stat)
 unique = data.nunique()
 
 print(unique)
+
+# Filter categorical variables
+categorical_columns = [x for x in data.dtypes.index if data.dtypes[x] == 'object']
+
+# Exclude ID cols and source:
+categorical_columns = [x for x in categorical_columns if x not in ['Item_Identifier', 'Outlet_Identifier', 'source']]
+
+# Print frequency of categories
+for col in categorical_columns:
+    print('\nFrequency of Categories for varible %s'%col)
+    print(data[col].value_counts())
+
+# Determine the average weight per item:
+item_avg_weight = data.pivot_table(values='Item_Weight', index='Item_Identifier')
+
+# Get a boolean variable specifying missing Item_Weight values
+miss_bool = data['Item_Weight'].isnull()
+
+# Impute data and check #missing values before and after imputation to confirm
+print('Orignal #missing: %d'% sum(miss_bool))
+data.loc[miss_bool,'Item_Weight'] = data.loc[miss_bool,'Item_Identifier'].apply(lambda x: item_avg_weight.at[x,'Item_Weight'])
+print('Final #missing: %d'% sum(data['Item_Weight'].isnull()))
+
+# Determing the mode for each
+outlet_size_mode = data.pivot_table(values='Outlet_Size', columns='Outlet_Type',aggfunc=(lambda x: x.mode().iat[0]) )
+print('Mode for each Outlet_Type:')
+print(outlet_size_mode)
+
+# Get a boolean variable specifying missing Item_Weight values
+miss_bool = data['Outlet_Size'].isnull()
+
+# Impute data and check #missing values before and after imputation to confirm
+print('\nOrignal #missing: %d'% sum(miss_bool))
+data.loc[miss_bool,'Outlet_Size'] = data.loc[miss_bool,'Outlet_Type'].apply(lambda x: outlet_size_mode[x])
+print(sum(data['Outlet_Size'].isnull()))
+
+# Determine average visibility of a product
+visibility_avg = data.pivot_table(values='Item_Visibility', index='Item_Identifier')
+
+# Impute 0 values with mean visibility of that product:
+miss_bool = (data['Item_Visibility'] == 0)
+
+print('Number of 0 values initially: %d'%sum(miss_bool))
+data.loc[miss_bool,'Item_Visibility'] = data.loc[miss_bool,'Item_Identifier'].apply(lambda x: visibility_avg.at[x,'Item_Visibility'])
+print('Number of 0 values after modification: %d'%sum(data['Item_Visibility'] == 0))
+
+# Determine another variable with means ratio
+data['Item_Visibility_MeanRatio'] = data.apply(lambda x: x['Item_Visibility']/visibility_avg[x['Item_Identifier']], axis=1)
+print(data['Item_Visibility_MeanRatio'].describe())
+
+# Get the first two characters of ID:
+data['Item_Type_Combined'] = data['Item_Identifier'].apply(lambda x: x[0:2])
+# Rename them to more intuitive categories:
+data['Item_Type_Combined'] = data['Item_Type_Combined'].map({'FD':'Food',
+                                                             'NC':'Non-Consumable',
+                                                             'DR':'Drinks'})
+new_cat = data['Item_Type_Combined'].value_counts()
+
+print(new_cat)
+
+# Years:
+data['Outlet_Years'] = 2013 - data['Outlet_Establishment_Year']
+data['Outlet_Years'].describe()
+
+# Change categories of low fat:
+print('Original Categories:')
+print (data['Item_Fat_Content'].value_counts())
+
+print('\nModified Categories:')
+data['Item_Fat_Content'] = data['Item_Fat_Content'].replace({'LF':'Low Fat',
+                                                             'reg':'Regular',
+                                                             'low fat':'Low Fat'})
+print(data['Item_Fat_Content'].value_counts())
+
+# Mark non-consumables as separate category in low_fat:
+data.loc[data['Item_Type_Combined'] == "Non-Consumable", 'Item_Fat_Content'] = "Non-Edible"
+print(data['Item_Fat_Content'].value_counts())
+
+le = LabelEncoder()
+# New variable for outlet
+data['Outlet'] = le.fit_transform(data['Outlet_Identifier'])
+var_mod = ['Item_Fat_Content','Outlet_Location_Type','Outlet_Size','Item_Type_Combined','Outlet_Type','Outlet']
+le = LabelEncoder()
+for i in var_mod:
+    data[i] = le.fit_transform(data[i])
+
+# One Hot Coding:
+data = pd.get_dummies(data, columns=['Item_Fat_Content','Outlet_Location_Type','Outlet_Size','Outlet_Type',
+                                     'Item_Type_Combined','Outlet'])
+
+# Drop the columns which have been converted to different types:
+data.drop(['Item_Type','Outlet_Establishment_Year'],axis=1,inplace=True)
+
+# Divide into test and train:
+train = data.loc[data['source']=="train"]
+test = data.loc[data['source']=="test"]
+
+# Drop unnecessary columns:
+test.drop(['Item_Outlet_Sales','source'],axis=1,inplace=True)
+train.drop(['source'],axis=1,inplace=True)
+
+# Export files as modified versions:
+train.to_csv("DataSet/bigmart/train_modified.csv",index=False)
+test.to_csv("DataSet/bigmart/test_modified.csv",index=False)
+
+#  Model Building
+
+# Mean based:
+mean_sales = train['Item_Outlet_Sales'].mean()
+
+print(mean_sales)
+
+# Define a dataframe with IDs for submission:
+base1 = test[['Item_Identifier','Outlet_Identifier']]
+base1['Item_Outlet_Sales'] = mean_sales
+
+# Define target and ID columns:
+target = 'Item_Outlet_Sales'
+IDcol = ['Item_Identifier', 'Outlet_Identifier']
+
+def modelfit(alg, dtrain, dtest, predictors, target, IDcol, filename):
+    # Fit the algorithm on the data
+    alg.fit(dtrain[predictors], dtrain[target])
+
+    # Predict training set:
+    dtrain_predictions = alg.predict(dtrain[predictors])
+
+    # Perform cross-validation:
+    cv_score = cross_validate.cross_val_score(alg, dtrain[predictors], dtrain[target], cv=20,
+                                                scoring='mean_squared_error')
+    cv_score = np.sqrt(np.abs(cv_score))
+
+    # Print model report:
+    print
+    "\nModel Report"
+    print
+    "RMSE : %.4g" % np.sqrt(metrics.mean_squared_error(dtrain[target].values, dtrain_predictions))
+    print
+    "CV Score : Mean - %.4g | Std - %.4g | Min - %.4g | Max - %.4g" % (
+    np.mean(cv_score), np.std(cv_score), np.min(cv_score), np.max(cv_score))
+
+    # Predict on testing data:
+    dtest[target] = alg.predict(dtest[predictors])
+
+    # Export submission file:
+    IDcol.append(target)
+    submission = pd.DataFrame({x: dtest[x] for x in IDcol})
+    submission.to_csv(filename, index=False)
